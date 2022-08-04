233f56a6f1633ae5dba0a0621c4bf3b1f7db1908
Julien Graziano <julien@graziano.fr>
2022-08-04 15:06:55 +0200

    build: add scripts to build/clean

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+export SOURCE_DIR="${SOURCE_DIR:-$PWD}"
+
+# shellcheck source=scripts/env.sh
+source "$SOURCE_DIR/scripts/env.sh"
+
+rm -rf "$BUILD_OUTPUT_DIR"
+
+npm install
+
+tsc \
+    --outDir "$BUILD_OUTPUT_DIR" \
+    --project "$SOURCE_DIR/src/lib/react-clock"
+
+node <<EOF
+const fs = require("fs")
+
+const package_json = JSON.parse(fs.readFileSync("$SOURCE_DIR/package.json", "utf8"))
+
+delete package_json.devDependencies
+delete package_json.private
+delete package_json.scripts
+
+fs.writeFileSync("$BUILD_OUTPUT_DIR/package.json", JSON.stringify(package_json, null, 2))
+EOF
