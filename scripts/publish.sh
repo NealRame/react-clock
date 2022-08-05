27e4ceaa9e7ba96befb6f4a0f0e8a569dca37970
Julien Graziano <julien@graziano.fr>
2022-08-05 16:25:01 +0200

    build: add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+export SOURCE_DIR="${SOURCE_DIR:-$PWD}"
+
+# shellcheck source=scripts/env.sh
+source "$SOURCE_DIR/scripts/env.sh"
+
+if [ -d "$BUILD_OUTPUT_DIR" ];
+then
+    npm publish "$BUILD_OUTPUT_DIR"
+fi
