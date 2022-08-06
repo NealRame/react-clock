f27639b69cc995c4b8e107878dd60b371b5018c6
Julien Graziano <julien@graziano.fr>
2022-08-06 16:49:47 +0200

    build: debug publish workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,8 +9,9 @@ export SOURCE_DIR="${SOURCE_DIR:-$PWD}"
 # shellcheck source=scripts/env.sh
 source "$SOURCE_DIR/scripts/build.sh"
 
-if [ -d "$BUILD_OUTPUT_DIR" ];
-then
-    # npm publish "$BUILD_OUTPUT_DIR" --access public
-    echo "$NODE_AUTH_TOKEN"
-fi
+echo "->${NODE_AUTH_TOKEN}<-"
+
+# if [ -d "$BUILD_OUTPUT_DIR" ];
+# then
+#     npm publish "$BUILD_OUTPUT_DIR" --access public
+# fi
