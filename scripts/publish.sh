3d2c6ca8f75eb5654246f7587c3cf8f1db766df5
Julien Graziano <julien@graziano.fr>
2022-08-06 17:02:46 +0200

    build: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,9 +9,9 @@ export SOURCE_DIR="${SOURCE_DIR:-$PWD}"
 # shellcheck source=scripts/env.sh
 source "$SOURCE_DIR/scripts/build.sh"
 
-echo "->${NODE_AUTH_TOKEN}<-"
+echo "->${NODE_AUTH_TOKEN: -8}<-"
 
-# if [ -d "$BUILD_OUTPUT_DIR" ];
-# then
-#     npm publish "$BUILD_OUTPUT_DIR" --access public
-# fi
+if [ -d "$BUILD_OUTPUT_DIR" ];
+then
+    npm publish "$BUILD_OUTPUT_DIR" --access public
+fi
