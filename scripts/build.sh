c5585ab8fac2ce97e273c31c3fbeac90cdf11d4b
Julien Graziano <julien@graziano.fr>
2022-08-06 22:02:26 +0200

    build: update build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,27 +11,6 @@ source "$SOURCE_DIR/scripts/env.sh"
 
 rm -rf "$BUILD_OUTPUT_DIR"
 
-npm install
-
 tsc \
     --outDir "$BUILD_OUTPUT_DIR" \
     --project "$SOURCE_DIR/src/lib/react-clock"
-
-pushd "$BUILD_OUTPUT_DIR"
-
-node <<EOF
-const fs = require("fs")
-
-const package_json = JSON.parse(fs.readFileSync("$SOURCE_DIR/package.json", "utf8"))
-
-delete package_json.devDependencies
-delete package_json.private
-delete package_json.scripts
-
-fs.writeFileSync("package.json", JSON.stringify(package_json, null, 2))
-EOF
-
-cp "$SOURCE_DIR/README.md" .
-cp "$SOURCE_DIR/LICENSE" .
-
-popd
