40037c6539f3b5d2595bb3b4e7dac0d3464db0d6
Julien Graziano <julien@graziano.fr>
2022-08-07 14:24:10 +0200

    build: generate npmrc file

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,3 +14,12 @@ rm -rf "$BUILD_OUTPUT_DIR"
 tsc \
     --outDir "$BUILD_OUTPUT_DIR" \
     --project "$SOURCE_DIR/src/lib/react-clock"
+
+# test if a variable is set to a non-empty value
+if [ -n "${NODE_AUTH_TOKEN:-}" ];
+then
+    cat > ".npmrc" <<EOF
+@nealrame:registry=https://registry.npmjs.org
+//registry.npmjs.org/:_authToken=$NODE_AUTH_TOKEN
+EOF
+fi
