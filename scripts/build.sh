55627bec43d6a6a8d04f2b3512ea4374a7063be6
Julien Graziano <julien@graziano.fr>
2022-08-08 16:21:42 +0200

    build: update build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,11 +15,27 @@ tsc \
     --outDir "$BUILD_OUTPUT_DIR" \
     --project "$SOURCE_DIR/src/lib/react-clock"
 
-# test if a variable is set to a non-empty value
-if [ -n "${NODE_AUTH_TOKEN:-}" ];
-then
-    cat > ".npmrc" <<EOF
-@nealrame:registry=https://registry.npmjs.org
+pushd "$BUILD_OUTPUT_DIR"
+
+cp "$SOURCE_DIR/LICENSE" .
+cp "$SOURCE_DIR/README.md" .
+
+cat > .npmrc <<EOF
+@nealrame:registry: https://registry.npmjs.org
 //registry.npmjs.org/:_authToken=$NODE_AUTH_TOKEN
 EOF
-fi
+
+node <<EOF
+const fs = require("fs")
+
+const package_json = JSON.parse(fs.readFileSync("$SOURCE_DIR/package.json"))
+
+delete package_json.scripts
+delete package_json.devDependencies
+
+package_json.main = "./index.js"
+
+fs.writeFileSync("package.json", JSON.stringify(package_json, null, 2))
+EOF
+
+popd
---
+++ scripts/publish.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+export SOURCE_DIR="${SOURCE_DIR:-$PWD}"
+
+# shellcheck source=scripts/env.sh
+source "$SOURCE_DIR/scripts/build.sh"
+
+pushd "$BUILD_OUTPUT_DIR"
+
+npm publish --access public
+
+popd
