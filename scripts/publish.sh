6a8051bb8a3c0c5cdf91c450cee1fca40c53e599
Julien Graziano <julien@graziano.fr>
2022-08-09 21:18:52 +0200

    build: debug GitHub publish workflow

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,14 +12,11 @@ source "$SOURCE_DIR/scripts/env.sh"
 pushd "$BUILD_OUTPUT_DIR"
 
 # display the 8 last characters of the npm access token
-echo "${NODE_AUTH_TOKEN:-"Undefined"} | tail -c 8"
+echo "${NODE_AUTH_TOKEN:-Undefined}" | tail -c 8
 # npm publish --access public
 
 node<<EOF
-for (const c of process.env.NODE_AUTH_TOKEN.slice(-7)) {
-    process.stdout.write(c)
-}
-process.stdout.write("\n")
+console.log("len(NODE_AUTH_TOKEN)", process.env.NODE_AUTH_TOKEN.length)
 EOF
 
 popd
